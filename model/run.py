d23f4a849c3613007965154f7cb0121d7cb90bcd
brianrqian <56141634+brianrqian@users.noreply.github.com>
2021-12-05 16:06:31 -0800

    update

--- model/run.py
+++ model/run.py
@@ -13,10 +13,6 @@ sys.path.insert(0, 'test')
 sys.path.insert(0, 'model')
 import LDA
 
-# import env_setup
-# from etl import get_data
-# from features import apply_features
-
 def main(targets):
     '''
     Runs the main project pipeline logic, given the targets.
@@ -24,10 +20,6 @@ def main(targets):
     
     `main` runs the targets in order of data=>analysis=>model.
     '''
-
-    # env_setup.make_datadir()
-    # env_setup.auth()
-
     #if 'test' in targets:
     #with open('test-params.json') as fh:
     #    data_cfg = json.load(fh)
@@ -37,26 +29,6 @@ def main(targets):
     data = 'final_hdsi_faculty_updated.csv'
     hdsi = 'HDSI.csv'
     LDA.LDA_model(data, hdsi)
-
-        # make the data target
-    #    df_test = pd.DataFrame.from_dict(data_cfg, orient="index").transpose()
-
-    #    print(df_test)
-
-    # if 'features' in targets:
-    #     with open('config/features-params.json') as fh:
-    #         feats_cfg = json.load(fh)
-
-    #     feats, labels = apply_features(data, **feats_cfg)
-
-    # if 'model' in targets:
-    #     with open('config/model-params.json') as fh:
-    #         model_cfg = json.load(fh)
-
-    #     # make the data target
-    #     model_build(feats, labels, **model_cfg)
-
-    #return df_test
 
 if __name__ == '__main__':
     # run via:
